d8b48ffd89df390d3b93ced6baf1c8c0d54b1673
philipng88 <philiplyng@gmail.com>
2020-02-06 17:28:30 -0700

    added movie seat booking project

---
+++ movieSeatBooking/app.js
@@ -0,0 +1,58 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
+populateUI();
+
+let ticketPrice = +movieSelect.value;
+
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+  const selectedSeatsCount = selectedSeats.length;
+  const seatsText = document.getElementById('seats-text');
+  const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+  selectedSeatsCount === 1
+    ? (seatsText.innerText = 'seat')
+    : (seatsText.innerText = 'seats');
+}
+
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) seat.classList.add('selected');
+    });
+  }
+  if (selectedMovieIndex !== null)
+    movieSelect.selectedIndex = selectedMovieIndex;
+}
+
+movieSelect.addEventListener('change', event => {
+  ticketPrice = +event.target.value;
+  setMovieData(event.target.selectedIndex, event.target.value);
+  updateSelectedCount();
+});
+
+container.addEventListener('click', event => {
+  if (
+    event.target.classList.contains('seat') &&
+    !event.target.classList.contains('occupied')
+  ) {
+    event.target.classList.toggle('selected');
+    updateSelectedCount();
+  }
+});
+
+updateSelectedCount();
