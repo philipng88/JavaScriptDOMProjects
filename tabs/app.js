7fc9818b6594890f9bd757576b7b6cf86f07aece
philipng88 <philiplyng@gmail.com>
2020-06-06 17:45:09 -0600

    added tabs project

---
+++ tabs/app.js
@@ -0,0 +1,17 @@
+const tabButtons = document.querySelectorAll('.tab-btn');
+const aboutContainer = document.getElementById('aboutContainer');
+const contentWrappers = document.querySelectorAll('.content');
+
+aboutContainer.addEventListener('click', event => {
+  const id = event.target.dataset.id;
+  if (id) {
+    tabButtons.forEach(tabButton => {
+      tabButton.classList.remove('active');
+      event.target.classList.add('active');
+    });
+    contentWrappers.forEach(contentWrapper =>
+      contentWrapper.classList.remove('active')
+    );
+    document.getElementById(id).classList.add('active');
+  }
+});
