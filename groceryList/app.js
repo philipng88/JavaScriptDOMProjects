ce18731dab012d858fec3c4dc610ff3ea3d684b2
philipng88 <philiplyng@gmail.com>
2020-06-08 02:02:58 -0600

    modified grocery list project

--- groceryList/app.js
+++ groceryList/app.js
@@ -1,96 +1,153 @@
-const addItemsAction = document.querySelector('.addItems-action');
-const input = document.querySelector('.addItems-input');
-const submit = document.querySelector('.addItems-submit');
+const alert = document.getElementById('alert');
+const groceryForm = document.getElementById('groceryForm');
+const groceryInput = document.getElementById('groceryInput');
+const submitBtn = document.getElementById('submitBtn');
+const groceryContainer = document.getElementById('groceryContainer');
+const groceryList = document.getElementById('groceryList');
+const clearBtn = document.getElementById('clearBtn');
 
-const list = document.querySelector('.grocery-list');
-const displayItemsAction = document.querySelector('.displayItems-action');
-const clear = document.querySelector('.displayItems-clear');
+const localStorageName = 'grocery-list-items';
+let editElement;
+let editFlag = false;
+let editId = '';
+
+const showAlert = (type, message) => {
+  alert.textContent = message;
+  alert.classList.add(`alert-${type}`);
+  setTimeout(() => {
+    alert.textContent = '';
+    alert.classList.remove(`alert-${type}`);
+  }, 2500);
+};
+
+const setBackToDefault = () => {
+  groceryInput.value = '';
+  editFlag = false;
+  editId = '';
+  submitBtn.textContent = 'add';
+};
+
+const getLocalStorage = () =>
+  localStorage.getItem(localStorageName)
+    ? JSON.parse(localStorage.getItem(localStorageName))
+    : [];
+
+const createListItem = (id, value) => {
+  const groceryItem = document.createElement('article');
+  const groceryItemId = document.createAttribute('data-id');
+  groceryItem.classList.add('grocery-item');
+  groceryItemId.value = id;
+  groceryItem.setAttributeNode(groceryItemId);
+  groceryItem.innerHTML = `
+    <p class='title'>${value}</p>
+    <div class='btn-container'>
+      <button type='button' class='edit-btn' title='Edit'>
+        <i class='fas fa-edit'></i>
+      </button>
+      <button type='button' class='delete-btn' title='Delete'>
+        <i class='fas fa-trash'></i>
+      </button>
+    </div>
+  `;
+  const deleteBtn = groceryItem.querySelector('.delete-btn');
+  const editBtn = groceryItem.querySelector('.edit-btn');
+  deleteBtn.addEventListener('click', event =>
+    handleItemAction(event, 'delete')
+  );
+  editBtn.addEventListener('click', event => handleItemAction(event, 'edit'));
+  groceryList.appendChild(groceryItem);
+};
+
+const setupItems = () => {
+  const items = getLocalStorage();
+  if (items.length) {
+    items.forEach(item => createListItem(item.id, item.value));
+    groceryContainer.classList.add('show-container');
+  }
+};
+
+const clearItems = () => {
+  const items = document.querySelectorAll('.grocery-item');
+  if (window.confirm('Remove all items from your grocery list?')) {
+    if (items.length) items.forEach(item => groceryList.removeChild(item));
+    groceryContainer.classList.remove('show-container');
+    showAlert('success', 'all items removed');
+    setBackToDefault();
+  }
+  localStorage.removeItem(localStorageName);
+};
+
+const handleLocalStorageAction = (actionType, id, value) => {
+  let items = getLocalStorage();
+  switch (actionType) {
+    case 'add':
+      items.push({ id, value });
+      break;
+    case 'edit':
+      items = items.map(item => {
+        if (item.id === id) item.value = value;
+        return item;
+      });
+      break;
+    case 'remove':
+      items = items.filter(item => {
+        if (item.id !== id) return item;
+      });
+      break;
+    default:
+      break;
+  }
+  localStorage.setItem(localStorageName, JSON.stringify(items));
+};
+
+const handleItemAction = (event, actionType) => {
+  const itemElement = event.currentTarget.parentElement.parentElement;
+  const itemId = itemElement.dataset.id;
+  const itemName =
+    event.currentTarget.parentElement.previousElementSibling.textContent;
+  switch (actionType) {
+    case 'delete':
+      groceryList.removeChild(itemElement);
+      if (!groceryList.children.length)
+        groceryContainer.classList.remove('show-container');
+      showAlert('success', `removed ${itemName} from the list`);
+      handleLocalStorageAction('remove', itemId);
+      setBackToDefault();
+      break;
+    case 'edit':
+      editElement = event.currentTarget.parentElement.previousElementSibling;
+      groceryInput.value = itemName;
+      editFlag = true;
+      editId = itemId;
+      submitBtn.textContent = 'update';
+    default:
+      break;
+  }
+};
 
 const addItem = event => {
   event.preventDefault();
-  let value = input.value;
-  if (value !== '') {
-    createItem(value);
-    updateStorage(value);
-    showAction(addItemsAction, `${value} added to the list`, 'success');
-  } else {
-    showAction(addItemsAction, 'please add a grocery item', 'alert');
-  }
-};
-
-const showAction = (element, text, status) => {
-  element.classList.add(`${status}`);
-  element.innerText = text;
-  input.value = '';
-  setTimeout(() => element.classList.remove(`${status}`), 3000);
-};
-
-const createItem = value => {
-  const groceryItemHTML = `
-    <div class="grocery-item">
-      <h4 class="grocery-item__title">${value}</h4>
-      <a href="#" class="grocery-item__link">
-        <i class="far fa-trash-alt" id="delete-icon"></i>
-      </a>
-    </div>
-    `;
-  list.insertAdjacentHTML('afterbegin', groceryItemHTML);
-};
-
-const updateStorage = value => {
-  let groceryList;
-  const groceryListStorage = localStorage.getItem('groceryList');
-  groceryList = groceryListStorage ? JSON.parse(groceryListStorage) : [];
-  groceryList.push(value);
-  localStorage.setItem('groceryList', JSON.stringify(groceryList));
-};
-
-const displayStorage = () => {
-  const groceryListStorage = localStorage.getItem('groceryList');
-  if (groceryListStorage) {
-    const storageItems = JSON.parse(groceryListStorage);
-    storageItems.forEach(element => {
-      createItem(element);
-    });
-  }
-};
-
-const removeItems = () => {
-  let groceryItems = document.querySelectorAll('.grocery-item');
-  if (groceryItems.length > 0) {
-    if (window.confirm('Are you sure you wish to remove all items?')) {
-      groceryItems.forEach(element => list.removeChild(element));
-      localStorage.getItem('groceryList') &&
-        localStorage.removeItem('groceryList');
-      showAction(displayItemsAction, 'all items removed', 'alert');
+  const value = groceryInput.value;
+  const id = new Date().getTime().toString();
+  if (value.trim() !== '') {
+    if (!editFlag) {
+      createListItem(id, value);
+      groceryContainer.classList.add('show-container');
+      showAlert('success', 'item added to list');
+      handleLocalStorageAction('add', id, value);
+      setBackToDefault();
+    } else {
+      editElement.textContent = value;
+      showAlert('success', 'item updated');
+      handleLocalStorageAction('edit', editId, value);
+      setBackToDefault();
     }
   } else {
-    showAction(displayItemsAction, 'there are no items to remove', 'alert');
+    showAlert('danger', 'please enter a grocery item');
+    groceryInput.value = '';
   }
 };
 
-const editStorage = item => {
-  const groceryItems = JSON.parse(localStorage.getItem('groceryList'));
-  const index = groceryItems.indexOf(item);
-  groceryItems.splice(index, 1);
-  localStorage.removeItem('groceryList');
-  localStorage.setItem('groceryList', JSON.stringify(groceryItems));
-};
-
-const removeSingleItem = event => {
-  event.preventDefault();
-  const link = event.target.parentElement;
-  if (link.classList.contains('grocery-item__link')) {
-    const text = link.previousElementSibling.innerHTML;
-    const groceryItem = event.target.parentElement.parentElement;
-    if (window.confirm(`Remove ${text}?`)) {
-      list.removeChild(groceryItem);
-      editStorage(text);
-    }
-  }
-};
-
-submit.addEventListener('click', addItem);
-document.addEventListener('DOMContentLoaded', displayStorage);
-clear.addEventListener('click', removeItems);
-list.addEventListener('click', removeSingleItem);
+groceryForm.addEventListener('submit', addItem);
+clearBtn.addEventListener('click', clearItems);
+window.addEventListener('DOMContentLoaded', setupItems);
