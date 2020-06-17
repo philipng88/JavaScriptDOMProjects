d0bdca3dcebc3f70aba28be84179db87c0c56b9f
philipng88 <philiplyng@gmail.com>
2020-06-17 13:59:39 -0600

    Quiz project: added validation for name input; modified logic for determining if current score makes the high score list

--- quiz/game-end.js
+++ quiz/game-end.js
@@ -1,33 +1,48 @@
 const name = document.getElementById('name');
+const alert = document.getElementById('alert');
 const finalScore = document.getElementById('finalScore');
-const saveScoreBtn = document.getElementById('saveScoreBtn');
 const highScoreForm = document.getElementById('highScoreForm');
 
 const mostRecentScore = localStorage.getItem('mostRecentScore') || 0;
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 const lowestHighScore = Math.min(...highScores.map(item => +item.score));
+const playersWithHighScore = highScores.map(item => item.name);
 const MAX_HIGH_SCORES = 5;
 
 finalScore.innerText = mostRecentScore;
 
-const saveHighScore = event => {
-  event.preventDefault();
-  const scoreData = { score: mostRecentScore, name: name.value };
+const saveHighScore = () => {
+  const scoreData = { score: mostRecentScore, name: name.value.trim() };
   highScores.push(scoreData);
   highScores.sort((a, b) => b.score - a.score).splice(MAX_HIGH_SCORES);
   localStorage.setItem('highScores', JSON.stringify(highScores));
-  window.location.assign('./index.html');
+  window.location.assign('./high-scores.html');
 };
 
-const addFormEventListeners = () => {
-  name.addEventListener('keyup', () => {
-    saveScoreBtn.disabled = !name.value.trim();
-  });
-  highScoreForm.addEventListener('submit', saveHighScore);
+const showAlert = message => {
+  alert.classList.remove('hidden');
+  alert.innerText = message;
+  setTimeout(() => {
+    alert.classList.add('hidden');
+    alert.innerText = '';
+  }, 3000);
 };
 
-if (highScores.length < MAX_HIGH_SCORES || mostRecentScore > lowestHighScore) {
-  addFormEventListeners();
-} else {
+if (
+  +mostRecentScore === 0 ||
+  (highScores.length >= MAX_HIGH_SCORES && +mostRecentScore <= lowestHighScore)
+)
   highScoreForm.remove();
-}
+
+highScoreForm.addEventListener('submit', event => {
+  event.preventDefault();
+  if (name.value.trim() !== '') {
+    if (!playersWithHighScore.includes(name.value.trim())) {
+      saveHighScore();
+    } else {
+      showAlert('Name is already in use');
+    }
+  } else {
+    showAlert('Please enter your name');
+  }
+});
